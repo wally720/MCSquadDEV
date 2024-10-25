320f4ada9472018377c28c5f5deffd8a0aba8778
wally720 <wally.spm@hotmail.com>
2024-10-25 15:09:58 -0500

    Update distromanager.js
    se actualiza el repo

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://drivesalvame.blob.core.windows.net/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://drivesalvame.blob.core.windows.net/repos/dev/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
