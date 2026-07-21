2f3d55d194b707688b33fadf4dec827d21e18d7a
wally720 <wally.spm@hotmail.com>
2026-07-21 10:35:54 -0500

    fix: update REMOTE_DISTRO_URL and increment version to 1.0.8

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,9 +2,7 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-// Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://drivesalvame.blob.core.windows.net/repos/dev/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://storage.googleapis.com/launcher-2f69b.firebasestorage.app/launcher/dev/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
